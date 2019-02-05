ca446800c34a31646490c980c631fac232524908
kvdprasad27 <kvdprasad27@gmail.com>
2019-02-05 16:53:34 +0530

    Create MyTestingJava.java

---
+++ src/main/java/com/git/test/MyTestingJava.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.git.test;
+
+/**
+ * @author vkankana
+ *
+ */
+public class MyTestingJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("GIT migration example MyTestingJavaMyTestingJava");
+	}
+
+}
